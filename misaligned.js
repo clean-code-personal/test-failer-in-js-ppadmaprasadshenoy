2aa927babaa267c193c17d07017082ff8f22b29e
P Padmaprasad Shenoy <92221430+ppadmaprasadshenoy@users.noreply.github.com>
2023-02-25 16:10:49 +0530

    Update misaligned.js

--- misaligned.js
+++ misaligned.js
@@ -1,10 +1,14 @@
 function print_color_map() {
-const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
-const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
-for (let i = 0; i < majorColors.length; i++) {
-    for (let j = 0; j < minorColors.length; j++) {
-        console.log(${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]});
+    const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
+    const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
+    for (let i = 0; i < majorColors.length; i++) {
+        for (let j = 0; j < minorColors.length; j++) {
+            console.log(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
+        }
     }
-  }
- return majorColors.length * minorColors.length;
+    return majorColors.length * minorColors.length;
 }
+
+module.exports = {
+  print_color_map
+};
