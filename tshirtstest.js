1013bec63ec49f4a3c66af8a602db4767967d633
P Padmaprasad Shenoy <92221430+ppadmaprasadshenoy@users.noreply.github.com>
2023-02-25 16:25:22 +0530

    Update tshirtstest.js

--- tshirtstest.js
+++ tshirtstest.js
@@ -4,10 +4,10 @@ const size = require('./tshirts');
 describe('size function', () => {
   it('should return S for values less than 38', () => {
     expect(size(37)).to.equal('S');
+    expect(size(38)).to.equal('S');
   });
 
   it('should return M for values between 38 and 42', () => {
-    expect(size(38)).to.equal('M');
     expect(size(40)).to.equal('M');
   });
 
