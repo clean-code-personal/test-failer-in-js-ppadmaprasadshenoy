f4b8eb32f7e226b2dc1a5d012e019cb32375f007
P Padmaprasad Shenoy <92221430+ppadmaprasadshenoy@users.noreply.github.com>
2023-02-25 16:30:52 +0530

    Update misalignedtest.js

--- misalignedtest.js
+++ misalignedtest.js
@@ -2,9 +2,9 @@ const {expect} = require('chai');
 const {print_color_map} = require('./misaligned');
 
 describe('print_color_map', function() {
-  it('should return 0 when there are no color codes printed', function() {
-    expect(print_color_map()).to.equal(0);
-  });
+//   it('should return 0 when there are no color codes printed', function() {
+//     expect(print_color_map()).to.equal(0);
+//   });
 
   it('should print 25 color codes and return 25', function() {
     expect(print_color_map()).to.equal(25);
