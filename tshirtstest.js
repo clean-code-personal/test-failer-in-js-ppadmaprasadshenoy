124f6584911daf3537e22906a13bc0b60593c962
P Padmaprasad Shenoy <92221430+ppadmaprasadshenoy@users.noreply.github.com>
2023-02-25 20:32:59 +0530

    Update tshirtstest.js

--- tshirtstest.js
+++ tshirtstest.js
@@ -1,18 +1,9 @@
 const chai = require('chai');
+const size = require('./tshirt');
+const expect = chai.expect;
 
-expect = chai.expect;
-
-describe('size', () => {
-  it('should return S when given 37', () => {
-    expect(size(37)).equals('S');
-  });
-  it('should return M when given 40', () => {
-    expect(size(40)).equals('M');
-  });
-  it('should return L when given null', () => {
-    expect(size(null)).equals('L');
-  });
-  it('should return L when given 43', () => {
-    expect(size(43)).equals('L');
-  });
-});
+expect(size(37)).equals('S');
+expect(size(38)).equals('M');
+expect(size(40)).equals('M');
+expect(size(43)).equals('L');
+console.log('All is well (maybe!)');
