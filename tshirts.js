3859f5bea4bf44795419e7cb183422fb498ac533
P Padmaprasad Shenoy <92221430+ppadmaprasadshenoy@users.noreply.github.com>
2023-02-25 18:29:19 +0530

    Update tshirts.js

--- tshirts.js
+++ tshirts.js
@@ -1,31 +1,13 @@
-// function size(cms) {
-//     if (cms < 38) {
-//         return 'S';
-//     } else if (cms > 38 && cms < 42) {
-//         return 'M';
-//     } else {
-//         return 'L';
-//     }
-// }
-
-// module.exports = size;
-
-const { expect } = require('chai');
-
 function size(cms) {
   if (cms < 38) {
     return 'S';
-  } else if (cms >= 38 && cms < 42) {
+  }  
+  else if (cms > 38 && cms < 42) {
     return 'M';
-  } else {
+  } 
+  else {
     return 'L';
   }
 }
 
-expect(size(37)).to.equal('S');
-expect(size(38)).to.equal('M');
-expect(size(40)).to.equal('M');
-expect(size(42)).to.equal('L');
-expect(size(43)).to.equal('L');
-
-console.log('All is well (maybe!)');
+module.exports = size;
