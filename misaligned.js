7b0491807d2485b499085c44b80494aa01f2f5a1
P Padmaprasad Shenoy <92221430+ppadmaprasadshenoy@users.noreply.github.com>
2023-02-26 21:12:06 +0530

    Update misaligned.js

--- misaligned.js
+++ misaligned.js
@@ -1,9 +1,16 @@
+function format(index, majorColor, minorColor) {
+    return `${index} | ${majorColor} | ${minorColor}`;
+}
+
 function print_color_map() {
     const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
     const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
     for (let i = 0; i < majorColors.length; i++) {
         for (let j = 0; j < minorColors.length; j++) {
-            console.log(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
+            const index = i * minorColors.length + j;
+            const majorColor = majorColors[i];
+            const minorColor = minorColors[j];
+            console.log(format(index, majorColor, minorColor));
         }
     }
     return majorColors.length * minorColors.length;
